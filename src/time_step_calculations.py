c337b537ee2e120b687da3a8f3ecb4dd03b8f32d
ConnorTalbs <connortalbot@live.com>
2022-11-12 13:02:05 -0500

    Co-authored-by: nilesjossel <nilesjossel@users.noreply.github.com> Co-authored-by: Costner Magill <CostnerMagill@users.noreply.github.com> Co-authored-by: danielbiggs00 <danielbiggs00@users.noreply.github.com>

--- src/time_step_calculations.py
+++ src/time_step_calculations.py
@@ -3,33 +3,42 @@ from route import *
 from sympy.solvers import solve
 from sympy import symbols
 
-t_step = 5 # seconds
-velocity = 65
+t_step = 5 # Seconds
+weight = 3790 # Newtons 
 
-while (velocity > 0):
-    pack_energy = 4500
-    current_velocity = velocity
+def main():
+    velocity = 100 # Kilometers per hour
+    while (velocity > 0):
+        pack_energy = 4500 # Watt Hours
+        current_velocity = velocity # in kph
 
-    while(pack_energy > 0):
-        delta_v = 0
-        #aero(KE)
-        #array(SOC)
-        #parasite(SOC)
-        #rolling_resistance(KE)
-        #motor(SOC, KE)
-    velocity -= 1
+        while(pack_energy > 0):
+            delta_KE = 0
+            delta_KE += aero()
+            #delta_KE += rolling_resistance()
+            pack_energy += array()
+            pack_energy += motor(delta_KE)
+            #pack_energy += parasitic()
+        velocity -= 1
 
-def aero(k_energy):
-    pass
+def aero():
+    air_density = (-3.64*(10**-14))*(elevation)**3+(3.88*(10**-9))*(elevation)**2-(1.18*(10**-4))*(elevation)+1.17 #air density in kg/m^3
+    aero_power = 0.5 * air_density * ((current_velociy*(1/3.6))**3) * drag_area #Drag power in watts
+    k_energy = (aero_power * t_step)/3600 #calculates watt-seconds and converts to watt-hours
+    return k_energy
 
 def array(pack_energy):
-    pass
+    return (700 * t_step / 3600) 
 
-def parasite(pack_energy):
-    pass
+# def parasitic():
+#     return (average_parasitic_power * t_step)
 
-def rolling_resistance(k_energy):
-    pass
+def rolling_resistance(current_velocity):
+    rr_coefficient = 0.0055 
+    rr_power = rr_coefficient * weight * (1 + (current_velocity/161)) # Watt hours
+    return rr_power
 
-def motor(pack_energy, k_energy):
-    pass
+def motor(delta_KE):
+    return (-delta_KE)
+
+main() 
